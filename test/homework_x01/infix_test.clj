81cd612e3e5315a35e6a32b4881ba6021a857b39
Daniel Solano Gómez <daniel@solanogomez.org>
2015-04-30 22:56:30 -0500

    Get rid of macroexpansion tests

--- test/homework_x01/infix_test.clj
+++ test/homework_x01/infix_test.clj
@@ -5,42 +5,14 @@
 
 (deftest test-infix
   (testing "Basic x op y expressions"
-    (testing "correct macro-expansion"
-      (is (= '(+ 1 1) (macroexpand '(infix 1 + 1))))
-      (is (= '(* 3 5) (macroexpand '(infix 3 * 5)))))
-    (testing "evaluation"
-      (is (= 2 (infix 1 + 1)))
-      (is (= 15 (infix 3 * 5)))))
+    (is (= 2 (infix 1 + 1)))
+    (is (= 15 (infix 3 * 5))))
   (testing "Recursive (x) op y expressions"
-    (testing "correct macro-expansion"
-      (is (= '(+ (homework-x01.infix/infix 1 - 2) 3) (macroexpand '(infix (1 - 2) + 3))))
-      (is (= '(+ (- 1 2) 3) (macroexpand-all '(infix (1 - 2) + 3))))
-      (is (= '(* (homework-x01.infix/infix (1 * 2) + 3) 4) (macroexpand '(infix ((1 * 2) + 3) * 4))))
-      (is (= '(* (+ (* 1 2) 3) 4) (macroexpand-all '(infix ((1 * 2) + 3) * 4)))))
-    (testing "evaluation"
-      (is (= 2 (infix (1 - 2) + 3)))
-      (is (= 20 (infix ((1 * 2) + 3) * 4)))))
+    (is (= 2 (infix (1 - 2) + 3)))
+    (is (= 20 (infix ((1 * 2) + 3) * 4))))
   (testing "Recursive x op (y) expressions"
-    (testing "correct macro-expansion"
-      (is (= '(-  1 (homework-x01.infix/infix 2 + 3)) (macroexpand '(infix 1 - (2 + 3)))))
-      (is (= '(- 1 (+ 2 3)) (macroexpand-all '(infix 1 - (2 + 3)))))
-      (is (= '(* 1 (homework-x01.infix/infix 2 + (3 * 4))) (macroexpand '(infix 1 * (2 + (3 * 4))))))
-      (is (= '(* 1 (+ 2 (* 3 4))) (macroexpand-all '(infix 1 * (2 + (3 * 4)))))))
-    (testing "evaluation"
-      (is (= -4 (infix 1 - (2 + 3))))
-      (is (= 14 (infix 1 * (2 + (3 * 4)))))))
+    (is (= -4 (infix 1 - (2 + 3))))
+    (is (= 14 (infix 1 * (2 + (3 * 4))))))
   (testing "Recursive (x) op (y) expressions"
-    (testing "correct macro-expansion"
-      (is (= '(- (homework-x01.infix/infix 1 * 2) (homework-x01.infix/infix 3 + 4))
-             (macroexpand '(infix (1 * 2) - (3 + 4)))))
-      (is (= '(- (* 1 2) (+ 3 4))
-             (macroexpand-all '(infix (1 * 2) - (3 + 4)))))
-      (is (= '(+ (homework-x01.infix/infix (1 * 2) - (3 + 4))
-                 (homework-x01.infix/infix (5 * 6) - (7 + 8)))
-             (macroexpand '(infix ((1 * 2) - (3 + 4)) + ((5 * 6) - (7 + 8))))))
-      (is (= '(+ (- (* 1 2) (+ 3 4))
-                 (- (* 5 6) (+ 7 8)))
-             (macroexpand-all '(infix ((1 * 2) - (3 + 4)) + ((5 * 6) - (7 + 8)))))))
-    (testing "evaluation"
-      (is (= -5 (infix (1 * 2) - (3 + 4))))
-      (is (= 10 (infix ((1 * 2) - (3 + 4)) + ((5 * 6) - (7 + 8))))))))
+    (is (= -5 (infix (1 * 2) - (3 + 4))))
+    (is (= 10 (infix ((1 * 2) - (3 + 4)) + ((5 * 6) - (7 + 8)))))))
